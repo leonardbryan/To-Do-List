d23050ba094041cb8fe2d5fcffd244d076a9043d
Leonard <leonard23bryan@gmail.com>
2022-02-21 10:58:32 +0800

    Added remove list function

--- index.js
+++ index.js
@@ -5,16 +5,29 @@
     let addButton = document.getElementById('addListButton')
     let listBody = document.getElementById('unorderListBody')
 
+    listBody.addEventListener('click', (e) => {
+        const  listItem = e.target;
+        console.log(listItem.classList[1])
+        if(listItem.classList[1] === 'bi-trash-fill'){
+            let todo = listItem.parentElement
+            let todoParent = todo.parentElement
+            let todoGrandParent = todoParent.parentElement
+            todoGrandParent.remove()
+        }
+        
+    })
+
     addButton.addEventListener('click', (e) => {
         if(inputValue.value != ""){
             e.preventDefault()
 
             let todoDiv = document.createElement('div')
             todoDiv.classList.add('todo')
-            
+             
             let newList = document.createElement('li')
 
             newList.classList.add('list-group-item')
+            newList.classList.add('todoItem')
             newList.classList.add('d-flex')
             newList.classList.add('justify-content-between')
             newList.classList.add('align-items-center')
@@ -35,6 +48,7 @@
 
             let completeIcon = document.createElement('i');
             completeIcon.innerHTML = '<i class="bi bi-file-earmark-check-fill"></i>'
+            completeIcon.classList.add('cIcon')
             completeIcon.classList.add('text-success')
             completeIcon.classList.add('me-4')
             completeIcon.classList.add('fs-5')
@@ -42,6 +56,7 @@
             
             let trashIcon = document.createElement('i');
             trashIcon.innerHTML = '<i class="bi bi-trash-fill"></i>'
+            trashIcon.classList.add('tIcon')
             trashIcon.classList.add('text-danger')
             trashIcon.classList.add('fs-5')
             listIconsContainer.appendChild(trashIcon) 
